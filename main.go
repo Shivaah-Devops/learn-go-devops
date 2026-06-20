6c431aa57313496826072c2cc28fcf947bb8f2ed
Shivaah-Devops <101786517+Shivaah-Devops@users.noreply.github.com>
2026-06-20 17:45:11 +1000

    Add files via upload

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	//fmt.Println("Welcome to booking app!")
+	//fmt.Println("here's the place to collect tickets")
+
+	var Confname = "GO Conf"
+	const noofTickets = 50
+	var reminaingtickets = 50
+
+	fmt.Printf("welcome to %v booking app!\n", Confname)
+	fmt.Printf("We have total of %v tickets and %v are still remaining.\n", noofTickets, reminaingtickets)
+	//fmt.Println("Name of the Conference: ", Confname)
+	//fmt.Println("Get your tickets here to attend")
+
+	//fmt.Println("Total no. of tickers:", noofTickets, "remaining tickets:", reminaingtickets)
+	fmt.Printf("Get your tickets from here to attend\n")
+
+	var userName string
+	var ticket int
+
+	userName = "Siva"
+	ticket = 2
+	fmt.Printf("user %v booked %v tickets\n", userName, ticket)
+
+}
